c5f2d5eaf29dea341b73cac8ec4c40ebfd95c3e3
Ryoji Uehara <72393752+u-na-gi@users.noreply.github.com>
2025-05-31 16:35:28 +0900

    Revert "add GET endpoints test"
    This reverts commit aa25a9272a196c71f421db5a918e084233dc581f.

--- app/backend/scenario-test/login.sf.ts
+++ app/backend/scenario-test/login.sf.ts
@@ -2,41 +2,37 @@ import { LoginRequest } from "./types/src/api/auth/login/login.ts"; // Import ty
 
 import { registerUser } from "./register-user.sf.ts";
 import { RegisterRequest } from "./types/src/api/user/register.ts";
-import { ScenarioFlow } from "scenario-flow";
 
-export const login = new ScenarioFlow("Login Frow", registerUser).step(
-  "Login User",
-  async (ctx) => {
-    const payload = ctx.getContext("user") as RegisterRequest;
-    const req: LoginRequest = {
-      username: payload.username,
-      password: payload.password,
-    };
-    const res = await ctx.fetcher({
-      method: "POST",
-      path: "/auth/login",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(req),
-    });
+export const login = registerUser.step(async (ctx) => {
+  const payload = ctx.getContext("user") as RegisterRequest;
+  const req: LoginRequest = {
+    username: payload.username,
+    password: payload.password,
+  };
+  const res = await ctx.fetcher({
+    method: "POST",
+    urlPaths: ["auth", "login"],
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
 
-    if (!res.ok) {
-      throw new Error(`Login failed: ${res.status} ${res.statusText}`);
-    }
-    const data = await res.json();
-
-    const cookies = res.headers.getSetCookie();
-
-    console.log("Login successful:", data);
-
-    ctx.addContext("cookies", cookies);
-    cookies.forEach((cookie) => {
-      const [name, value] = cookie.split(";")[0].split("=");
-      ctx.addContext(name, `${name}=${value}`);
-    });
+  if (!res.ok) {
+    throw new Error(`Login failed: ${res.status} ${res.statusText}`);
   }
-);
+  const data = await res.json();
+
+  const cookies = res.headers.getSetCookie();
+
+  console.log("Login successful:", data);
+
+  ctx.addContext("cookies", cookies);
+  cookies.forEach((cookie) => {
+    const [name, value] = cookie.split(";")[0].split("=");
+    ctx.addContext(name, `${name}=${value}`);
+  });
+});
 
 if (import.meta.main) {
   await login.execute();
